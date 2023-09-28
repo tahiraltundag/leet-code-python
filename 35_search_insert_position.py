b4cfb164a9b3576d5a4bd48484b0fa7308f4ced1
Tahir Altundağ <98649118+tahiraltundag@users.noreply.github.com>
2023-09-28 21:30:19 +0300

    Update

--- 35_search_insert_position.py
+++ 35_search_insert_position.py
@@ -1,7 +1,1 @@
-def search_insert_position(nums, val):
-    replace = 0
-    for i in range(len(nums)):
-        if nums[i] != val:
-            nums[replace] = nums[i]
-            replace += 1
-    return replace
+
