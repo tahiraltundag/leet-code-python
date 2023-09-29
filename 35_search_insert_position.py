6d3ed2f180db4abc172970c99009170bd2eb17be
Tahir Altundağ <98649118+tahiraltundag@users.noreply.github.com>
2023-09-29 22:34:37 +0300

    initial commit

--- 35_search_insert_position.py
+++ 35_search_insert_position.py
@@ -1,1 +1,17 @@
+nums = [1,2,4,5,7,8]
+target = 7
 
+def search_insert(nums, target):
+    l = 0
+    r = len(nums)-1
+    while l <= r:
+        mid = (l + r) // 2
+        if nums[mid] < target:
+            l = mid + 1
+        elif nums[mid] > target:
+            r = mid - 1
+        else:
+            return mid
+    return l
+
+search_insert(nums, target)
